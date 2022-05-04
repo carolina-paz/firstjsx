03b687cbd96f2e0e90cd39f9f30680bdfbc906fe
carolina-paz <97477596+carolina-paz@users.noreply.github.com>
2022-05-04 12:35:34 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Revisit Python</li>
+        <li>Order a burger</li>
+        <li>Take a nap</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
